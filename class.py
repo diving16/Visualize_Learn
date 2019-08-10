4ce1d29aa3e1fdc81595a426ea1c96e0624ba687
diving16 <daiweiying@seu.edu.cn>
2019-08-10 16:10:20 +0800

    Create class.py

---
+++ class.py
@@ -0,0 +1,24 @@
+class User:
+    pass
+
+user1=User()
+user1.first_name='Xukun'
+user1.last_name='Cai'
+user1.age='19'
+
+print(user1.first_name,user1.last_name,user1.age)
+
+first_name='Yifan'
+last_name='Wu'
+
+print(first_name,last_name)
+
+user2=User()
+user2.first_name='Han'
+user2.last_name='Lu'
+user2.age='28'
+user2.occupation='idol'
+
+print(user2.first_name,user2.last_name,user2.age,user2.occupation)
+
+
