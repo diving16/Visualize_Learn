2c884c08fa0a0691d45d8ccca87c957a90eab9b9
diving16 <daiweiying@seu.edu.cn>
2019-08-10 10:13:43 +0800

    data_visual by loading csv file

--- data_visual.py
+++ data_visual.py
@@ -1,18 +1,30 @@
 from matplotlib import pyplot as plt
 import pandas as pd
 
-x=[1,2,3,4,5,6,7,8,9,10]
-y=[1,3,5,6,7,8,9,10,13,17]
-z=[2,4,6,8,10,11,12,14,16,20]
+#x=[1,2,3,4,5,6,7,8,9,10]
+#y=[1,3,5,6,7,8,9,10,13,17]
+#z=[2,4,6,8,10,11,12,14,16,20]
 
-plt.plot(x,y,'x')
-plt.plot(x,z,'o')
-plt.legend(['y legend','z legend'])
+#导入外部数据
+data1=pd.read_csv('sample_data.csv') 
 
-plt.xlabel('x')
-plt.ylabel('y and z')
-plt.title('Two plots')
+print(data1)
+print(type(data1))
 
-plt.show()
+#选取表格中某个数据
+number=data1.column_b.iloc[8]
+number2=data1.iloc[3,1]
+
+print(number)
+print(number2)
+#plt.plot(x,y,'x')
+#plt.plot(x,z,'o')
+#plt.legend(['y legend','z legend'])
+
+#plt.xlabel('x')
+#plt.ylabel('y and z')
+#plt.title('Two plots')
+
+#plt.show()
 
 
